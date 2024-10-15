097f78db4648a53481fc01ff15a49f53e9a70d63
originsmatthew <mjn2710@gmail.com>
2024-10-15 14:37:12 +0100

    Update script.js

--- bonus/script.js
+++ bonus/script.js
@@ -10,9 +10,9 @@ function generateImage() {
     habboImage2.src = `https://habboden.com/habbo-imaging/${habbo2}?size=b&action=wav&frame=2`;
 
     // Position the images correctly behind the holiday romance
-    habboImage1.style.left = '15px'; // Adjusted for better alignment
+    habboImage1.style.left = '10px'; // Adjusted for better alignment
     habboImage1.style.bottom = '-100px'; // Lowered for better fit
-    habboImage2.style.left = '48px'; // Adjust based on your design
+    habboImage2.style.left = '28px'; // Adjust based on your design
     habboImage2.style.bottom = '-83px'; // Lowered for better fit
 
     // Show the Habbo images
